902905ead0ec9a3e2659b0bdc8e6ed656b4ea2fd
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-04-22 16:25:29 +0400

    del same site

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -172,7 +172,6 @@ const login = (req, res, next) => {
             .cookie('jwt', token, {
               maxAge: 3600000,
               httpOnly: true,
-              sameSite: true,
             })
             .status(200).send({ user: sendUser });
         })
