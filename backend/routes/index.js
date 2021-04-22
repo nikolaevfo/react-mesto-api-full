1f8865e8b845ffeaf2b2b065f994ed7b9b0d0bcc
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-04-22 17:22:25 +0400

    del http only

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-
+const cookieParser = require('cookie-parser');
 const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./users');
 const { createUser, login } = require('../controllers/users');
 const cardRouter = require('./cards');
 const auth = require('../middlewares/auth');
 
+router.use(cookieParser());
 router.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -18,7 +19,6 @@ router.post('/signin', celebrate({
     password: Joi.string().required().min(6),
   }),
 }), login);
-
 router.use(auth);
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
