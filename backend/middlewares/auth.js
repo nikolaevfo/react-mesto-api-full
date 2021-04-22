9b3cca16d7d0a2a58172cfc98dcf7e88a48d4f59
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-04-22 16:36:35 +0400

    del same site

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -14,7 +14,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(res.status(401).send({ message: `Авторизация не прошла ${JSON.stringify(token)}` }));
+    next(res.status(401).send({ message: `Авторизация не прошла ${JSON.stringify(req)}` }));
   }
 
   req.user = payload;
