596e4aac0b0f1fabf8656cf8b406a7888ff1c3e6
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-04-22 17:18:09 +0400

    del http only

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 const app = express();
-app.use(cookieParser());
+// app.use(cookieParser());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
