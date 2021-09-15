f26ca4085b6af5acaf02fa83d4ab1725c412835e
Fedor Nikolaev <nikolaevfo@gmail.com>
2021-09-15 09:14:01 +0400

    debug cookie settings for development

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,14 +1,14 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const validator = require('validator');
-const User = require('../models/user');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+const User = require("../models/user");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const NotFoundError = require('../errors/not-found-err');
-const RequestError = require('../errors/req-err');
-const AuthError = require('../errors/auth-err');
-const MongoError = require('../errors/mongo-err');
+const NotFoundError = require("../errors/not-found-err");
+const RequestError = require("../errors/req-err");
+const AuthError = require("../errors/auth-err");
+const MongoError = require("../errors/mongo-err");
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -19,56 +19,65 @@ const getUsers = (req, res, next) => {
 const getUserById = (req, res, next) => {
   const { id } = req.params;
   User.findById(id)
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
+    .then(
+      (user) => {
+        if (!user) {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        }
+        res.send(user);
+      },
+      (err) => {
+        if (err.message === "NotValidId") {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        } else if (err.name === "CastError") {
+          throw new RequestError("Переданы некорректные данные");
+        }
       }
-      res.send(user);
-    }, (err) => {
-      if (err.message === 'NotValidId') {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
-      } else if (err.name === 'CastError') {
-        throw new RequestError('Переданы некорректные данные');
-      }
-    })
+    )
     .catch(next);
 };
 
 const getUser = (req, res, next) => {
   const id = req.user._id;
   User.findById(id)
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
+    .then(
+      (user) => {
+        if (!user) {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        }
+        res.send(user);
+      },
+      (err) => {
+        if (err.message === "NotValidId") {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        } else if (err.name === "CastError") {
+          throw new RequestError("Переданы некорректные данные");
+        }
       }
-      res.send(user);
-    }, (err) => {
-      if (err.message === 'NotValidId') {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
-      } else if (err.name === 'CastError') {
-        throw new RequestError('Переданы некорректные данные');
-      }
-    })
+    )
     .catch(next);
 };
 
 const updateUser = (req, res, next) => {
   const data = { ...req.body };
   User.findByIdAndUpdate(req.user._id, data, { new: true, runValidators: true })
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
+    .then(
+      (user) => {
+        if (!user) {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        }
+        res.send(user);
+      },
+      (err) => {
+        if (err.message === "NotValidId") {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        } else if (err.name === "CastError") {
+          throw new RequestError("Переданы некорректные данные");
+        } else if (err.name === "ValidationError") {
+          throw new RequestError("Ошибка валидации");
+        }
       }
-      res.send(user);
-    }, (err) => {
-      if (err.message === 'NotValidId') {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
-      } else if (err.name === 'CastError') {
-        throw new RequestError('Переданы некорректные данные');
-      } else if (err.name === 'ValidationError') {
-        throw new RequestError('Ошибка валидации');
-      }
-    })
+    )
     .catch(next);
 };
 
@@ -76,56 +85,64 @@ const updateAvatar = (req, res, next) => {
   const data = { ...req.body };
 
   if (!validator.isURL(data.avatar)) {
-    throw new RequestError('Ссылка неверная');
+    throw new RequestError("Ссылка неверная");
   }
 
-  User.findByIdAndUpdate(req.user._id, data, { new: true, runValidators: true, upsert: true })
-    .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Пользователь не найден');
+  User.findByIdAndUpdate(req.user._id, data, {
+    new: true,
+    runValidators: true,
+    upsert: true,
+  })
+    .then(
+      (user) => {
+        if (!user) {
+          throw new NotFoundError("Пользователь не найден");
+        }
+        res.send(user);
+      },
+      (err) => {
+        if (err.message === "NotValidId") {
+          throw new NotFoundError("Запрашиваемый пользователь не найден");
+        } else if (err.name === "CastError") {
+          throw new RequestError("Запрашиваемый пользователь не найден");
+        } else if (err.name === "ValidationError") {
+          throw new RequestError("Ошибка валидации");
+        }
       }
-      res.send(user);
-    }, (err) => {
-      if (err.message === 'NotValidId') {
-        throw new NotFoundError('Запрашиваемый пользователь не найден');
-      } else if (err.name === 'CastError') {
-        throw new RequestError('Запрашиваемый пользователь не найден');
-      } else if (err.name === 'ValidationError') {
-        throw new RequestError('Ошибка валидации');
-      }
-    })
+    )
     .catch(next);
 };
 
 const createUser = (req, res, next) => {
-  const {
-    name, about, avatar, email, password,
-  } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
   return User.findOne({ email })
     .then((user) => {
       if (user) {
-        throw new MongoError('Пользователь уже существует');
+        throw new MongoError("Пользователь уже существует");
       } else {
-        bcrypt.hash(password, 10)
-          .then((hash) => {
-            User.create({
-              name, about, avatar, email, password: hash,
+        bcrypt.hash(password, 10).then((hash) => {
+          User.create({
+            name,
+            about,
+            avatar,
+            email,
+            password: hash,
+          })
+            .then((newUser) => {
+              if (!newUser) {
+                throw new RequestError("Ошибка валидации");
+              }
+              const sendUser = {
+                name: newUser.name,
+                about: newUser.about,
+                avatar: newUser.avatar,
+                email: newUser.email,
+              };
+              res.status(201).send({ user: sendUser });
             })
-              .then((newUser) => {
-                if (!newUser) {
-                  throw new RequestError('Ошибка валидации');
-                }
-                const sendUser = {
-                  name: newUser.name,
-                  about: newUser.about,
-                  avatar: newUser.avatar,
-                  email: newUser.email,
-                };
-                res.status(201).send({ user: sendUser });
-              })
-              .catch(next);
-          });
+            .catch(next);
+        });
       }
     })
     .catch(next);
@@ -134,21 +151,22 @@ const createUser = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
-  return User.findOne({ email }).select('+password')
+  return User.findOne({ email })
+    .select("+password")
     .then((user) => {
       if (!user) {
-        throw new AuthError('Неправильные почта или пароль');
+        throw new AuthError("Неправильные почта или пароль");
       }
       return bcrypt
         .compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            throw new AuthError('Неправильные почта или пароль');
+            throw new AuthError("Неправильные почта или пароль");
           }
           const token = jwt.sign(
             { _id: user._id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-            { expiresIn: '7d' },
+            NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+            { expiresIn: "7d" }
           );
           const sendUser = {
             name: user.name,
@@ -157,13 +175,14 @@ const login = (req, res, next) => {
             email: user.email,
           };
           res
-            .cookie('jwt', token, {
+            .cookie("jwt", token, {
               maxAge: 3600000,
               httpOnly: true,
-              sameSite: 'None',
-              secure: true,
+              sameSite: "None",
+              secure: NODE_ENV === "production",
             })
-            .status(200).send({ user: sendUser });
+            .status(200)
+            .send({ user: sendUser });
         })
         .catch(next);
     })
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,16 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const AuthError = require('../errors/auth-err');
+const AuthError = require("../errors/auth-err");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
+  console.log(req);
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(
+      token,
+      NODE_ENV === "production" ? JWT_SECRET : "dev-secret"
+    );
   } catch (err) {
-    throw new AuthError('Авторизация не прошла');
+    throw new AuthError(err);
   }
 
   req.user = payload;
